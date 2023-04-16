0a9d8814eaff419531ffa2619e994c4e76945b81
elisanoelia <enurbanom@outlook.com>
2023-04-15 22:40:26 -0400

    agregando img

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 //obtener elementos de la tabla
 var tabla=document.getElementById("tabla");
 var contenido=document.getElementById("contenido");
+var carta=document.getElementById("carta");
 //consumo de api con fetch
 console.log(tabla);
 console.log(contenido);
@@ -23,6 +24,39 @@ function mostrarTabla(data){
     <td>${temp.name}</td>
     <td>${temp.level}</td>`
       
-   }
+}}
+//para mostrar en la sectiob id carta
+ function mostrarCarta(){
+let busqueda = document.getElementById("busqueda").value.toLowerCase();
+let tabla = document.getElementById("tabla")
+let contenido = document.getElementById("contenido");
+if (tabla != null && contenido != null){
+ tabla.style.display = "none"
+contenido.style.display ="none"
 
 }
+    fetch(`https://digimon-api.vercel.app/api/digimon/name/${busqueda}`)
+    .then(response=>response.json())
+    .then(datos=>{
+        console.log(datos);
+        construirCarta(datos)
+    })}
+
+    function construirCarta(param){
+ /*  document.getElementById("tabla").style.display="none";
+     document.getElementById("contenido").style.display="none"; */
+       tabla.innerHTML=""
+       contenido.innerHTML=""
+       carta.innerHTML=""
+       for(let temp of param){
+     
+        carta.innerHTML+=
+        ` <div class="card col-6 mx-auto" style="width:18rem">
+        <img src="${temp.img}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${temp.name}</h5>
+          <p class="card-text">${temp.level}</p>
+        </div>`
+       }
+    
+    }
